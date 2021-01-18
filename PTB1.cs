69905dd0833b46f0871b5427de1abc56a4af119a
phambichluu <77492760+phambichluu@users.noreply.github.com>
2021-01-18 20:47:56 +0700

    Add files via upload

---
+++ PTB1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPTB1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap a vao: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Nhap b vao: ");
+            double b = double.Parse(Console.ReadLine());
+            if (a == 0)
+            {
+                if (b == 0)
+                    Console.WriteLine("Phương trinh vo so nghiem:");
+                else
+                    Console.WriteLine("Phuong trinh vo nghiem:");
+            }
+            else
+            {
+                double x = -b / a;
+                Console.WriteLine("Phuong trinh có nghiem la: " + x);
+            }
+        }
+    }
+
+}
